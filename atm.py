d43903a760661bc55f4f6ec39cc43bf505a4f216
Samy Mohsen <samy.mohsen@live.com>
2018-03-05 04:48:09 +0200

    done with atm class

---
+++ atm.py
@@ -0,0 +1,33 @@
+class ATM():
+	def __init__(self,balance,bank_name):
+		self.balance = balance
+		self.bank_name = bank_name
+
+	def withdraw(self,a):
+		if a <= self.balance:
+			print 'Welcome To : ' + self.bank_name
+			print 'Current Balance : ' + str(self.balance)
+			print '================================='
+			while a > 0:
+				if a >= 100:
+					print 'give 100'
+					a -= 100
+				elif a >= 50:
+					print 'give 50'
+					a -= 50
+				elif a >= 20:
+					print 'give 20'
+					a -= 20
+				elif a >= 10:
+					print 'give 10'
+					a -= 10
+				elif a >= 5:
+					print 'give 5'
+					a -= 5
+				elif a < 5:
+					print 'give 1'
+					a -= 1
+		else:
+			print 'Welcome to : ' + self.bank_name
+			print '================================='
+			print "NO SUFFICIENT FUNDS"
